01018e6951923f5f6bd33ab06cc5342c9c412751
Devin Chen <d3vin.chen@gmail.com>
2018-08-24 01:38:43 +0800

    Add pagination

--- src/List.js
+++ src/List.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react';
 import { Table } from 'antd';
 
+const dataSource = [
+  {
+    key: '1',
+    name: 'Mike'
+  },
+  {
+    key: '2',
+    name: 'John 2'
+  },
+  {
+    key: '3',
+    name: 'John 3'
+  },
+  {
+    key: '4',
+    name: 'John 4'
+  },
+  {
+    key: '5',
+    name: 'John 5'
+  },
+  {
+    key: '6',
+    name: 'John 6'
+  },
+  {
+    key: '7',
+    name: 'John 7'
+  },
+  {
+    key: '8',
+    name: 'John 8'
+  }
+];
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name'
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age'
+  }
+];
+
 class App extends Component {
   render() {
     return (
       <div>
-        <Table />
+        <Table
+          dataSource={dataSource}
+          columns={columns}
+          pagination={{
+            pageSize: 2
+          }}
+        />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 
+import 'antd/dist/antd.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
